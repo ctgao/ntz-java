6566f1108c9c2beb4e30de3517c77184837a06bf
Christine <christine@zipcodersMBP13.localdomain>
2023-07-13 18:55:43 -0400

    wrote add - it should work for the remember and create a category

--- ntz/src/main/java/rocks/zipcode/Notez.java
+++ ntz/src/main/java/rocks/zipcode/Notez.java
@@ -12,11 +12,11 @@ public final class Notez {
     }
     /**
      * Says hello to the world.
-     * 
+     *
      * @param args The arguments of the program.
      */
     public static void main(String argv[]) {
-        boolean _debug = true;
+        boolean _debug = false;
         // for help in handling the command line flags and data!
         if (_debug) {
             System.err.print("Argv: [");
@@ -32,14 +32,16 @@ public final class Notez {
 
         /*
          * You will spend a lot of time right here.
-         * 
+         *
          * instead of loadDemoEntries, you will implement a series
          * of method calls that manipulate the Notez engine.
          * See the first one:
          */
         ntzEngine.loadDemoEntries();
-
-        ntzEngine.saveDatabase();
+        // TESTING
+//        argv = "-r \"Another note\"".split(" ");
+//        argv = "-c \"note2\" \"testing Notes\"".split(" ");
+        argv = "-f \"note2\" 1".split(" ");
 
         if (argv.length == 0) { // there are no commandline arguments
             //just print the contents of the filemap.
@@ -49,14 +51,37 @@ public final class Notez {
                 ntzEngine.addToCategory("General", argv);
             } // this should give you an idea about how to TEST the Notez engine
               // without having to spend lots of time messing with command line arguments.
+            else if(argv[0].equals("-c")){
+                // creates and/or appends to a category
+                ntzEngine.addToCategory(argv[1], argv);
+            }
+            else if(argv[0].equals("-f")){
+                // forgets a note
+                ntzEngine.removeFromCategory(argv[1], argv);
+            }
+            else if(argv[0].equals("-e")){
+                // edit or replace a note
+            }
+            ntzEngine.printResults();
         }
         /*
          * what other method calls do you need here to implement the other commands??
          */
-
+        ntzEngine.saveDatabase();
     }
 
-    private void addToCategory(String string, String[] argv) {
+    private void addToCategory(String category, String[] argv) {
+        // parse the argv first
+        String noteText = removeQuotes(parseNote(category, argv));
+        category = removeQuotes(category);
+
+        // string is ready to be added to a note list
+        if(!filemap.containsKey(category)){
+            filemap.put(category, new NoteList(noteText));
+        }
+        else{
+            filemap.get(category).add(noteText);
+        }
     }
 
     private void saveDatabase() {
@@ -80,5 +105,39 @@ public final class Notez {
     /*
      * Put all your additional methods that implement commands like forget here...
      */
+    private void removeFromCategory(String category, String[] argv) {
+        // parse the argv first
+        String noteText = removeQuotes(parseNote(category, argv));
+        category = removeQuotes(category);
 
+        // string is ready to be added to a note list
+        filemap.get(category).remove(noteText);
+        if(filemap.get(category).isEmpty()){
+            filemap.remove(category);
+        }
+    }
+
+    private String parseNote(String category, String[] input){
+        // parse the argv first
+        StringBuilder sb = new StringBuilder();
+        boolean quoteFound = false;
+        for(String s : input){
+            if(s.contains(category)){
+                continue;
+            }
+            quoteFound = (s.indexOf('\"') != -1) || quoteFound;
+            if(quoteFound){
+                sb.append(s);
+                sb.append(" ");
+            }
+        }
+        // delete the extra space
+        sb.deleteCharAt(sb.lastIndexOf(" "));
+
+        return sb.toString();
+    }
+
+    private String removeQuotes(String in){
+        return in.replace("\"", "");
+    }
 }
